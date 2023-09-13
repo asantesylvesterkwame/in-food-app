7825f9f4215f8ff575be208a73e01dfeb8b9e762
asantesylvesterkwame <intelligentson17@gmail.com>
2023-09-13 04:58:27 +0000

    Created Component

---
+++ infood/screens/NavigationDrawerScreens/Resturant.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { colors } from "../../globals/styles";
+
+const Resturant = () => {
+    return (
+        <View ></View>
+    )
+};
+
+
+export default Resturant
