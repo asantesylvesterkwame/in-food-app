55bcca06388655bb7998ab9f238f2757a15c1c6d
asantesylvesterkwame <intelligentson17@gmail.com>
2023-09-13 04:59:24 +0000

    imported AppHeader Component

--- infood/screens/Home.js
+++ infood/screens/Home.js
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View, Platform, Dimensions } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { createStackNavigator } from "@react-navigation/stack";
+import AppHeader from "../components/AppHeader";
+import { Header } from "react-navigation-stack";
+
 
 const Stack = createStackNavigator();
 
@@ -8,9 +11,25 @@ export default function Home({ navigation }) {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <View>
-          <Text>Home</Text>
-        </View>
+        {/* <Header
+          leftComponent={{
+            icon: "menu",
+            color: "#fff",
+            onPress: () => navigation.openDrawer(),
+          }}
+          centerComponent={{
+            text: "Home",
+            style: { color: "#fff" },
+          }}
+        /> */}
+        <AppHeader>
+          <View></View>
+          <View>
+            <Text style={{ fontSize: 20 }}>Home</Text>
+          </View>
+          <View></View>
+        </AppHeader>
+        <View></View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
