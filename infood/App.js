2b75ca3d559170bbf515013713b2df76c68f9123
asantesylvesterkwame <intelligentson17@gmail.com>
2023-09-24 18:54:50 +0000

    added Data Peristence, added resturant and resturant datails page

--- infood/App.js
+++ infood/App.js
@@ -5,14 +5,31 @@ import OnBoarding from "./screens/OnBoarding";
 import Register from "./screens/Register";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
-import ProfileSetup from "./screens/ProfileSetup";
+import ProfilePage from "./screens/ProfilePage";
 import { StatusBar } from "expo-status-bar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import { Auth } from "./firebase.config";
 
 import "./firebase.config";
+import { useEffect, useState } from "react";
+import ResturantDetails from "./screens/ResturantDetails";
 
 const Stack = createStackNavigator();
+
 export default function App() {
- 
+  // const [user, setUser] = useState({});
+  const [user] = useAuthState(Auth);
+  useEffect(() => {
+    // onAuthStateChanged(Auth, (user) => {
+    //   setUser(user);
+    //   console.log("current user", user);
+    // });
+  }, []);
+  // const authState = ()=>{
+  //   onAuthStateChanged(Auth.currentUser, (currentUser) =>{
+  // })
+
   return (
     <NavigationContainer>
       {/* <AppDrawerNavigator/> */}
@@ -23,7 +40,7 @@ export default function App() {
         style="light"
       />
 
-      <Stack.Navigator initialRouteName="OnBoarding">
+      <Stack.Navigator initialRouteName={user ? "Home" : "OnBoarding"}>
         <Stack.Screen
           name="Home"
           options={{
@@ -31,6 +48,14 @@ export default function App() {
             headerShown: false,
           }}
           component={Home}
+        />
+        <Stack.Screen
+          name="ResturantDetails"
+          options={{
+            // Hide the header for all other routes.
+            headerShown: false,
+          }}
+          component={ResturantDetails}
         />
         <Stack.Screen
           name="OnBoarding"
@@ -50,12 +75,12 @@ export default function App() {
           component={Login}
         />
         <Stack.Screen
-          name="ProfileSetup"
+          name="ProfilePage"
           options={{
             // Hide the header for all other routes.
             headerShown: false,
           }}
-          component={ProfileSetup}
+          component={ProfilePage}
         />
       </Stack.Navigator>
     </NavigationContainer>
