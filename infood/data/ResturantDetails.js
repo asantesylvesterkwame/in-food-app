845a954ab52e4499fb2333ab395e63beb62a8eda
asantesylvesterkwame <intelligentson17@gmail.com>
2023-09-25 04:58:29 +0000

    created dummy data

---
+++ infood/data/ResturantDetails.js
@@ -0,0 +1,47 @@
+import foodOne from "../assets/foodone.jpg";
+import foodtwo from "../assets/foodtwo.jpg";
+
+export const resturantDetailsData = [
+  {
+    id: 1,
+    foodName: "Chicken Pizza",
+    foodTags: ["Chicken", "Pizza"],
+    Price: 60.0,
+    image: foodOne,
+  },
+  {
+    id: 2,
+    foodName: "Banku And Okro Stew",
+    foodTags: ["Banku", "Okro"],
+    Price: 80.0,
+    image: foodtwo,
+  },
+  {
+    id: 3,
+    foodName: "Tea & Bread",
+    foodTags: ["Tea", "Bread"],
+    Price: 40.0,
+    image: foodOne,
+  },
+  {
+    id: 4,
+    foodName: "Meat Pizza",
+    foodTags: ["Meat", "Pizza"],
+    Price: 100.0,
+    image: foodOne,
+  },
+  {
+    id: 5,
+    foodName: "Fish Pie",
+    foodTags: ["Banku", "Okro"],
+    Price: 80.0,
+    image: foodtwo,
+  },
+  {
+    id: 6,
+    foodName: "Fufu & Groundut Soup",
+    foodTags: ["Banku", "Okro"],
+    Price: 80.0,
+    image: foodtwo,
+  },
+];
